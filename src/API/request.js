f71cc75fd62366857512ce081ee113f7d91187b5
AnimXer01 <157260909+AnimXer01@users.noreply.github.com>
2024-03-03 22:09:31 +0800

    Update request.js

--- src/API/request.js
+++ src/API/request.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://animxer-api-seven.vercel.app/anime/gogoanime";
+const baseUrl = "https://consumet-api-eysu.onrender.com/anime/gogoanime";
 
 export const makeRequest = async (endpoint, config) => {
     const url = `${baseUrl}${endpoint}`;
